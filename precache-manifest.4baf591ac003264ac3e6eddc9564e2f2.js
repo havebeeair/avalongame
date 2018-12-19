1b258df65d56fb525c92ab4ab92946e765db336a
havebeeair <arq.javierriver@gmail.com>
2018-12-19 18:21:23 -0300

    Updates

---
+++ precache-manifest.4baf591ac003264ac3e6eddc9564e2f2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/avalongame/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "5dd05a7456d0200a1881",
+    "url": "/avalongame/static/js/runtime~main.5dd05a74.js"
+  },
+  {
+    "revision": "bbd21a691a2ad0ad5a08",
+    "url": "/avalongame/static/js/main.bbd21a69.chunk.js"
+  },
+  {
+    "revision": "324b6cf3b56bf43378ce",
+    "url": "/avalongame/static/js/1.324b6cf3.chunk.js"
+  },
+  {
+    "revision": "bbd21a691a2ad0ad5a08",
+    "url": "/avalongame/static/css/main.457c378a.chunk.css"
+  },
+  {
+    "revision": "d2216c4f024024a6801348deaef7b25b",
+    "url": "/avalongame/index.html"
+  }
+];
